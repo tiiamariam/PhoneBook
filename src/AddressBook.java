5f0a1b1ac2b33902344465570c9ebb096cadf714
Tiia <tiia.korpi@gmail.com>
2021-06-10 00:17:38 +0300

    Delete src directory

--- src/AddressBook.java
+++
@@ -1,8 +0,0 @@
-
-//Main method, runs the menu from Controller class
-public class AddressBook {
-    public static void main(String[] args) {
-        Controller controller = new Controller("data_file.txt");
-        controller.run();
-    }
-}
